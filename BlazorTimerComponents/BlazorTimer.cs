eea2ee33f79739dcdd6a6dabb1a9c6a75aeb9f82
Michael Puckett <Michael.Puckett@tripleg3.com>
2022-06-01 12:07:17 -0400

    Init commit. This has an example in the README and is functional as is.

---
+++ BlazorTimerComponents/BlazorTimer.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel;
+
+namespace BlazorTimerComponents;
+public class BlazorTimer : ComponentBase, IDisposable, INotifyPropertyChanged
+{
+    private readonly Timer timer;
+    private int interval = Timeout.Infinite;
+    private bool isRunning;
+
+    public event PropertyChangedEventHandler? PropertyChanged = default!;
+
+    public BlazorTimer()
+    {
+        timer = new Timer(async state => await InvokeAsync(async () => await Ellapsed.InvokeAsync()), null, Timeout.Infinite, Timeout.Infinite);
+        PropertyChanged += BlazorTimer_PropertyChanged;
+    }
+
+    [Parameter]
+    public EventCallback Ellapsed { get; set; } = default!;
+
+    [Parameter]
+    public int Interval
+    {
+        get => interval;
+        set
+        {
+            if (value == interval) return;
+            interval = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Interval)));
+        }
+    }
+
+    [Parameter]
+    public bool IsRunning
+    {
+        get => isRunning;
+        set
+        {
+            if (value == IsRunning) return;
+            isRunning = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsRunning)));
+        }
+    }
+
+    private void BlazorTimer_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        switch (e.PropertyName)
+        {
+            case nameof(IsRunning):
+                if (IsRunning) timer.Change(0, Interval);
+                else timer.Change(Timeout.Infinite, Timeout.Infinite);
+                break;
+            case nameof(Interval):
+                if (IsRunning) timer.Change(0, Interval);
+                break;
+        }
+    }
+
+    public void Dispose()
+    {
+        timer.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    ~BlazorTimer() => Dispose();
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # BlazorTimer
 A blazor timer component to make handling timed events easier.
+
+Easy to use:
+
+    <BlazorTimer Interval=@interval Ellapsed=@Ellapsed IsRunning=@isRunning />
+
+    @code {
+        private int interval;
+        private bool isRunning;
+        private void Ellapsed() => //some logic
+    }
+
+Try this view in Blazor to experiment:
+    @page "/"
+    <PageTitle>Blazor Timer</PageTitle>
+    <p role="status">Current count: @currentCount</p>
+    <button class="btn btn-primary" @onclick="ResetCount">Reset</button>
+    <input type="number" @bind-value=@interval />
+    <button class="btn btn-primary" @onclick=@(x => isRunning = true)>Start Timer</button>
+    <button class="btn btn-primary" @onclick=@(x => isRunning = false)>Stop Timer</button>
+    <BlazorTimer Interval=@interval Ellapsed=@BlazorTimerEllapsed IsRunning=@isRunning/>
+    @code {
+        private int currentCount = 0;
+        private int interval = Timeout.Infinite;
+        private bool isRunning;
+        private void BlazorTimerEllapsed() => currentCount++;
+        private void ResetCount() => currentCount = 0;
+    }
